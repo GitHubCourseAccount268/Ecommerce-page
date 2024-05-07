39769dca1771e629700aee444fe16242abd16499
Saideepthi <ballani.saideepthi@gmail.com>
2024-05-07 03:48:01 -0700

    countrysearch project

---
+++ countrysearch/countrysearch.js
@@ -0,0 +1,68 @@
+let inputEl = document.getElementById("searchInput");
+let spinnerEl = document.getElementById("spinner");
+let countriesEl = document.getElementById("resultCountries");
+let searchInputValue = "";
+let countriesList = [];
+
+function createAndAppendCountry(country) {
+    let countryEl = document.createElement("div");
+    countryEl.classList.add("country-card", "col-11", "col-md-5", "mr-auto", "ml-auto", "d-flex", "flex-row");
+    countriesEl.appendChild(countryEl);
+
+    let countryFlagEl = document.createElement("img");
+    countryFlagEl.src = country.flag;
+    countryFlagEl.classList.add("country-flag", "mt-auto", "mb-auto");
+    countryEl.appendChild(countryFlagEl);
+
+    let countryInfoEl = document.createElement("div");
+    countryInfoEl.classList.add("d-flex", "flex-column", "ml-4");
+    countryEl.appendChild(countryInfoEl);
+
+    let countryNameEl = document.createElement("p");
+    countryNameEl.textContent = country.name;
+    countryNameEl.classList.add("country-name");
+    countryInfoEl.appendChild(countryNameEl);
+
+    let countryPopulationEl = document.createElement("p");
+    countryPopulationEl.textContent = country.population;
+    countryPopulationEl.classList.add("country-population");
+    countryInfoEl.appendChild(countryPopulationEl);
+}
+
+function displaySearchResults() {
+    countriesEl.textContent = "";
+    for (let country of countriesList) {
+        // console.log(country);
+        let countryName = country.name;
+        if (countryName.includes(searchInputValue)) {
+            createAndAppendCountry(country);
+        }
+    }
+}
+
+function getCountries() {
+    let url = "https://apis.ccbp.in/countries-data";
+    let options = {
+        method: "GET"
+    };
+    spinnerEl.classList.remove("d-none");
+    fetch(url, options).
+    then(function(response) {
+        return response.json();
+    }).
+    then(function(jsonData) {
+        spinnerEl.classList.add("d-none");
+        countriesList = jsonData;
+        displaySearchResults();
+    });
+}
+
+function changesearchInput(event) {
+    if (event.key === "Enter") {
+        searchInputValue = event.target.value;
+        displaySearchResults();
+    }
+
+}
+getCountries();
+inputEl.addEventListener("keydown", changesearchInput);
